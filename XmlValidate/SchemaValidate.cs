fbe199944968db70f6434c4b9820b7b19961aa17
Jeffery Cosby <jcoz1968@hotmail.com>
2018-07-30 12:24:18 -0500

    initial

---
+++ XmlValidate/SchemaValidate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XmlValidate
+{
+    public class SchemaValidate
+    {
+        public SchemaValidate()
+        {
+        }
+
+        public bool ValidateSchema(string xml, string schema)
+        {
+            try
+            {
+                XmlReader reader;
+                XmlReaderSettings settings = new XmlReaderSettings();
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+
+                schemaSet.Add(null, schema);
+                //settings.ValidationEventHandler += new ValidationEventHandler()
+
+                settings.ValidationType = ValidationType.Schema;
+                settings.Schemas = schemaSet;
+                settings.DtdProcessing = DtdProcessing.Ignore;
+
+                reader = XmlReader.Create(xml, settings);
+
+
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
